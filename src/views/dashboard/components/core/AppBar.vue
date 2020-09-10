d2978dc2ffd27b2b51860e20475bcd20be68c36e
Ikhsan G <ikhsanguntara22@gmail.com>
2020-09-10 11:13:11 +0700

    a

--- src/views/dashboard/components/core/AppBar.vue
+++ src/views/dashboard/components/core/AppBar.vue
@@ -1,48 +1,45 @@
 <template>
-      <!-- App Bars -->
-    <v-app-bar app color="primary" dark clipped-left clipped-right>
-      <v-app-bar-nav-icon @click="setDrawer(!drawer)" />
-        <v-toolbar-title style="minn-width: 150px">Latihan API</v-toolbar-title>
-        <v-spacer />
-           <div style="width:320px">
-        <v-text-field
-          flat
-          dense
-          solo-inverted
-          hide-details
-          prepend-inner-icon="mdi-magnify"
-          label="Search"
-          class="hidden-sm-and-down"
-        />
-      </div>
-      <v-btn class="ml-1" icon>
-        <v-icon>mdi-apps</v-icon>
-      </v-btn>
-    </v-app-bar>
-    <!-- App Bars -->
+  <!-- App Bars -->
+  <v-app-bar app absolute color="primary">
+    <v-app-bar-nav-icon @click="setDrawer(!drawer)" />
+    <v-toolbar-title style="minn-width: 150px">Latihan API</v-toolbar-title>
+    <v-spacer />
+    <div style="width:320px">
+      <v-text-field
+        flat
+        dense
+        solo-inverted
+        hide-details
+        prepend-inner-icon="mdi-magnify"
+        label="Search"
+        class="hidden-sm-and-down"
+      />
+    </div>
+    <v-btn class="ml-1" icon>
+      <v-icon>mdi-apps</v-icon>
+    </v-btn>
+  </v-app-bar>
+  <!-- App Bars -->
 </template>
 
 <script>
-import { mapState, mapMutations } from 'vuex'
+import { mapState, mapMutations } from "vuex";
 
 export default {
-name: 'AppBar',
-data:() => ({
+  name: "AppBar",
+  data: () => ({}),
 
-}),
+  computed: {
+    ...mapState(["drawer"]),
+  },
 
-    computed: {
-      ...mapState(['drawer']),
-    },
-
-    methods: {
-      ...mapMutations({
-        setDrawer: 'SET_DRAWER',
-      }),
-    },
-}
+  methods: {
+    ...mapMutations({
+      setDrawer: "SET_DRAWER",
+    }),
+  },
+};
 </script>
 
 <style>
-
 </style>
